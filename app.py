7bc282843635c31295e9fadfb1a317a16d44a490
Pranjal <pranjal357kumar@gmail.com>
2026-01-08 13:11:27 +0530

    Model training

--- app.py
+++ app.py
@@ -7,15 +7,28 @@ from src.mlproject.components.data_transformation import (
     DataTransformationConfig,
     DataTransformation,
 )
+from src.mlproject.components.model_trainer import ModelTrainer, ModelTrainerConfig
 
 
 if __name__ == "__main__":
-    logging.info("execution has started")
+    logging.info("The execution has started")
+
     try:
+        # data_ingestion_config=DataIngestionConfig()
         data_ingestion = DataIngestion()
         train_data_path, test_data_path = data_ingestion.initiate_data_ingestion()
+
+        # data_transformation_config=DataTransformationConfig()
         data_transformation = DataTransformation()
-        data_transformation.initiate_data_transormation(train_data_path, test_data_path)
+        train_arr, test_arr, _ = data_transformation.initiate_data_transormation(
+            train_data_path, test_data_path
+        )
+
+        ## Model Training
+
+        model_trainer = ModelTrainer()
+        print(model_trainer.initiate_model_trainer(train_arr, test_arr))
+
     except Exception as e:
-        logging.info("Handling custom exception")
+        logging.info("Custom Exception")
         raise CustomException(e, sys)
